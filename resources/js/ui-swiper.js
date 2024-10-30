5fad363cdb55f1bdcd477aa20c8633aa652009b1
jineeds <jineeds94@gmail.com>
2024-10-30 17:02:48 +0900

    category section - js(autoplay) 적용 / img 전체 삽입 / 카드 레이아웃 변경

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -1,37 +1,43 @@
 var swiper = new Swiper('.slide-intro', {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,
-  // autoplay: true,
-  pagination: {
-    el: '.slide-intro .swiper-pagination',
-    clickable: true,
-  },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    // autoplay: true,
+    pagination: {
+        el: '.slide-intro .swiper-pagination',
+        clickable: true,
+    },
 });
 
 var swiper = new Swiper('.slide-studio', {
-  slidesPerView: 1.2,
-  spaceBetween: 20,
-  pagination: {
-    el: '.slide-studio .swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    600: {
-      slidesPerView: 2.2,
-      spaceBetween: 20,
+    slidesPerView: 1.2,
+    spaceBetween: 20,
+    pagination: {
+        el: '.slide-studio .swiper-pagination',
+        clickable: true,
     },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
+    breakpoints: {
+        600: {
+            slidesPerView: 2.2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
     },
-  },
+    loop: true, // 슬라이더가 무한 반복되도록 설정
+    autoplay: {
+        delay: 1500, // 슬라이드 전환 간격(밀리초 단위, 3000ms = 3초)
+        disableOnInteraction: false, // 사용자가 슬라이드를 클릭해도 자동 전환 유지
+    },
+    speed: 1000, // 슬라이드가 넘어가는 속도 (1초)
 });
 
 var swiper = new Swiper('.slide-best', {
-  spaceBetween: 20,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-  },
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+    },
 });
